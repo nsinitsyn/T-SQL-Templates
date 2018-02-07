ad27da804314c4abbd52f90fb9502aa29ad2d442
Nikita Sinitsyn <Nikita Sinitsyn>
2018-02-08 00:54:25 +0300

    add stats.sql

---
+++ stats.sql
@@ -0,0 +1,21 @@
+﻿DBCC SHOW_STATISTICS(N'Sales.Orders',N'idx_nc_empid') WITH HISTOGRAM;
+
+DBCC SHOW_STATISTICS(N'Sales.Orders',N'idx_nc_empid') WITH STAT_HEADER;
+
+-- Удаление автоматически созданной статистики
+DECLARE @statistics_name AS NVARCHAR(128), @ds AS NVARCHAR(1000);
+DECLARE acs_cursor CURSOR FOR
+SELECT name AS statistics_name
+FROM sys.stats
+WHERE object_id = OBJECT_ID(N'Sales.Orders', N'U')
+AND auto_created = 1;
+OPEN acs_cursor;
+FETCH NEXT FROM acs_cursor INTO @statistics_name;
+WHILE @@FETCH_STATUS = 0
+BEGIN
+SET @ds = N'DROP STATISTICS Sales.Orders.' + @statistics_name +';';
+EXEC(@ds);
+FETCH NEXT FROM acs_cursor INTO @statistics_name;
+END;
+CLOSE acs_cursor;
+DEALLOCATE acs_cursor;
